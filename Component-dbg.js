07e516af1e26e25cf81bfab12913fb4cb1ffb447
dishapalan02 <81086047+dishapalan02@users.noreply.github.com>
2022-06-08 23:41:52 +0530

    Add files via upload

---
+++ Component-dbg.js
@@ -0,0 +1,45 @@
+sap.ui.define([
+        "sap/ui/core/UIComponent",
+        "sap/ui/Device",
+        "sap/cp/fsmextension/model/models"
+    ],
+    function (UIComponent, Device, models) {
+        "use strict";
+
+        return UIComponent.extend("sap.cp.fsmextension.Component", {
+            metadata: {
+                manifest: "json"
+            },
+
+            /**
+             * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
+             * @public
+             * @override
+             */
+            init: function () {
+                // call the base component's init function
+                UIComponent.prototype.init.apply(this, arguments);
+
+                // Shell SDK library
+                sap.ui.loader.config({
+                    paths: {
+                        "sap/coresystems/fsm-shell": "https://unpkg.com/fsm-shell@1.5.1/release/fsm-shell-client"
+                    },
+                    shim: {
+                        "sap/coresystems/fsm-shell": {
+                            amd: true,
+                            exports: "FSMShell"
+                        }
+                    },
+                    async: true
+                });
+
+                // enable routing
+                this.getRouter().initialize();
+
+                // set the device model
+                this.setModel(models.createDeviceModel(), "device");
+            }
+        });
+    }
+);
